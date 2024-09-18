c4ddcf8ac0da2a77ce90d94277bd24b66eccc1e5
Adan Rodriguez <adan.rodriguez.fusta@gmail.com>
2024-09-18 17:17:50 -0300

    Add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
+  adapter: vercel(),
   integrations: [
     tailwind({
       // Example: Permite escribir declaraciones CSS anidadas
