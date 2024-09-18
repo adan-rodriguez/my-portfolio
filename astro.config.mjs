0d61786fddf097b91bfb0a1089ac5b98d2b94129
Adan Rodriguez <adan.rodriguez.fusta@gmail.com>
2024-09-18 17:17:12 -0300

    Add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "hybrid",
   integrations: [
     tailwind({
       // Example: Permite escribir declaraciones CSS anidadas
