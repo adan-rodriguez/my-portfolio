a7d9125484497a0993749d587fa8b2ea48fc94f0
Adan Rodriguez <adan.rodriguez.fusta@gmail.com>
2025-04-04 02:30:41 -0300

    migro icon techs a svg sprite

--- src/utils/techs.js
+++ src/utils/techs.js
@@ -1,82 +1,82 @@
 export const TECHNOLOGIES = {
   TYPESCRIPT: {
     name: "Typescript",
-    image: "typescript.svg",
+    image: "typescript",
     color: { bg: "rgb(49 121 198 / 40%)", text: "rgb(248 250 252)" },
   },
   NEXTJS: {
     name: "Next.js",
-    image: "nextjs.svg",
+    image: "nextjs",
     color: { bg: "rgb(0 0 0 / 50%)", text: "rgb(248 250 252)" },
   },
   FIREBASE: {
     name: "Firebase",
-    image: "firebase.svg",
+    image: "firebase",
     color: { bg: "rgb(165 39 20 / 50%)", text: "rgb(248 250 252)" },
   },
   CLOUDINARY: {
     name: "Cloudinary",
-    image: "cloudinary.svg",
+    image: "cloudinary",
     color: { bg: "rgb(52 72 197 / 20%)", text: "rgb(248 250 252)" },
   },
   TAILWIND: {
     name: "Tailwind CSS",
-    image: "tailwind.svg",
+    image: "tailwind",
     color: { bg: "rgb(56 189 248 / 30%)", text: "rgb(248 250 252)" },
   },
   REDUX_TOOLKIT: {
     name: "Redux Toolkit",
-    image: "redux.svg",
+    image: "redux",
     color: { bg: "rgb(118 74 188 / 20%)", text: "rgb(248 250 252)" },
   },
   BOOTSTRAP: {
     name: "Bootstrap",
-    image: "bootstrap.svg",
+    image: "bootstrap",
     color: { bg: "rgb(102 16 242 / 25%)", text: "rgb(248 250 252)" },
   },
   HTML: {
     name: "HTML",
-    image: "html.svg",
+    image: "html",
     color: { bg: "rgb(227 79 38 / 30%)", text: "rgb(248 250 252)" },
   },
   CSS: {
     name: "CSS",
-    image: "css.svg",
+    image: "css",
     color: { bg: "rgb(12 115 184 / 40%)", text: "rgb(248 250 252)" },
   },
   JAVASCRIPT: {
     name: "Javascript",
-    image: "javascript.svg",
+    image: "javascript",
     color: { bg: "rgb(247 223 30 / 30%)", text: "rgb(248 250 252)" },
   },
   REACT: {
     name: "React",
-    image: "react.svg",
+    image: "react",
     color: { bg: "rgb(97 218 251 / 30%)", text: "rgb(248 250 252)" },
   },
   ASTRO: {
     name: "Astro",
-    image: "astro.svg",
+    image: "astro",
     color: { bg: "rgb(216 51 51 / 30%)", text: "rgb(248 250 252)" },
   },
   GOOGLE_SHEETS: {
     name: "Google Sheets",
-    image: "google-sheets.svg",
+    image: "google-sheets",
     color: { bg: "rgb(52 168 83 / 30%)", text: "rgb(248 250 252)" },
   },
   SUPABASE: {
     name: "Supabase",
-    image: "supabase.svg",
+    image: "supabase",
     color: { bg: "rgb(0 98 57 / 30%)", text: "rgb(248 250 252)" },
   },
   TANSTACK_QUERY: {
     name: "TanStack Query",
-    image: "tanstack.svg",
+    image: "tanstack",
     color: { bg: "rgb(238 117 29 / 30%)", text: "rgb(248 250 252)" },
   },
   SWIPER: {
     name: "Swiper",
-    image: "swiper.svg",
+    image: "swiper",
     color: { bg: "rgb(0 128 255 / 30%)", text: "rgb(248 250 252)" },
   },
 };
